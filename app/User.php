35bddeeb93f6b5456b439711533bd460a4449b6c
adding table <zangjiahou@hotmail.com>
2018-11-25 15:32:52 -0500

    Added user profile migrate,model, and updated user seeder to create profiles.

--- app/User.php
+++ app/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(App\User::class, 50)->create()->each(function ($user) {
-           // $user->profile()->save(factory(App\Profile::class)->make());
+            $user->profile()->save(factory(App\Profile::class)->make());
         });
     }
 }
