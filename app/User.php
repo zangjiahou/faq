d63d966c891c072f97372777bcabd0af3d5ddcbc
adding table <zangjiahou@hotmail.com>
2018-11-25 16:19:07 -0500

    Added Question migrate,model, and updated user seeder and added tests for user profile, and question test

--- app/User.php
+++ app/User.php
@@ -56,4 +56,8 @@ class User extends Authenticatable
     {
         return $this->hasOne('App\Profile');
     }
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
 }
